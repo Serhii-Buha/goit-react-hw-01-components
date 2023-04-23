36a7914fa26084eaf5d72558d2dae3d2a10b7e3b
Serhii-Buha <serhii.buga@gmail.com>
2023-04-23 18:32:16 +0300

    add components Profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
+import user from './profile/user.json';
+import Profile from './profile/Profile';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+      </div>
+    </>
   );
 };
