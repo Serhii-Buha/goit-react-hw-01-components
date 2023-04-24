c61c85abe915f4edfb6c8388b0607b41a1f63454
Serhii-Buha <serhii.buga@gmail.com>
2023-04-24 20:40:09 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,7 @@ export const App = () => {
           display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
-          fontSize: 40,
+          // fontSize: 40,
           color: '#010101',
         }}
       >
