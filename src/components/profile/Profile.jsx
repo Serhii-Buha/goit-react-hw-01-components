8b80a44ece3449228cd9727e6fcf943cdfc70177
Serhii-Buha <serhii.buga@gmail.com>
2023-04-29 15:00:59 +0300

    update src import

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -35,6 +35,8 @@ export const Profile = ({ username, tag, location, avatar, stats }) => {
   );
 };
 
+// export default Profile;
+
 Profile.propTypes = {
   username: PropTypes.string.isRequired,
   tag: PropTypes.string.isRequired,
